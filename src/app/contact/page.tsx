eecb0785e179bf5693aae692a4693a3f478b3777
GargDivanshu <divanshu.garg113@gmail.com>
2023-09-17 15:52:33 +0530

    minor err

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,7 @@
-export default function Page() {
 import Navbar from './../components/Navbar';
+
+export default function Page() {
+
     return (
         <main className="bg-white relative flex min-h-screen flex-col items-center md:px-36 px-6 md:pb-36">
         <Navbar/>
