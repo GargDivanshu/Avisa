7bc4423a9e4bc30acb0831fed7bb4be854bda73f
GargDivanshu <divanshu.garg113@gmail.com>
2023-09-17 16:29:04 +0530

    inter err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'], weights: ['100', '200', '300', '400'] })
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Avisa',
