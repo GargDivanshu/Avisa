ad27aa67cafc9b1f3c8ea0b4f287ae646bb36b03
GargDivanshu <divanshu.garg113@gmail.com>
2023-09-24 12:38:50 +0530

    push

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,143 +1,143 @@
 import React, { FC } from "react";
-import Image from 'next/image'
-import Link from 'next/link'
-import {fadeIn, staggerContainer, textVariant} from './../utils/motion'
-import {motion} from 'framer-motion'
+import Image from "next/image";
+import Link from "next/link";
+import { fadeIn, staggerContainer, textVariant } from "./../utils/motion";
+import { motion } from "framer-motion";
 
 const About: FC = () => {
   return (
     <div className="text-black shadow-md rounded-md md:w-full w-full flex flex-col justify-center text-center">
-
-<motion.div 
-variants={staggerContainer}
-initial="hidden"
-whileInView="show"
-// viewport={{ once: false, amount: 0.1 }}
-className="md:text-5xl text-3xl font-bold h-fit my-auto text-primary">
-  <motion.p
-  // variants={fadeIn('up', 'tween', 0.2, 1)}
-  >
-    About Us
-  </motion.p>
-  
-  </motion.div>
-      <motion.div 
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      // viewport={{ once: false, amount: 0.1 }}
-      className="md:text-3xl text-2xl font-bold h-fit my-auto text-primary">
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        // viewport={{ once: false, amount: 0.1 }}
+        className="md:text-5xl text-3xl font-bold h-fit my-auto text-primary"
+      >
         <motion.p
-        className="py-8"
         // variants={fadeIn('up', 'tween', 0.2, 1)}
-        >Who We Are ?
-        </motion.p></motion.div>
-
-      <motion.div 
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      // viewport={{ once: false, amount: 0.1 }}
-      className="md:col-span-2 text-gray-500 p-2">
-       <motion.p
-         variants={fadeIn('up', 'tween', 0.1, 0.9)}
-       >
-
-At Avisa Swasthya, we are dedicated to transforming healthcare through
-        innovation and compassion. With our advanced diagnostic solutions, we
-        ensure accurate and accessible testing for all. From comprehensive
-        prenatal and postnatal care to health education and community outreach,
-        our mission is to empower individuals, families, and communities with
-        personalized healthcare solutions for a healthier future.
-       </motion.p>
-      
+        >
+          About Us
+        </motion.p>
+      </motion.div>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        // viewport={{ once: false, amount: 0.1 }}
+        className="md:text-3xl text-2xl font-bold h-fit my-auto text-primary"
+      >
+        <motion.p
+          className="py-8"
+          // variants={fadeIn('up', 'tween', 0.2, 1)}
+        >
+          Who We Are ?
+        </motion.p>
       </motion.div>
 
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        // viewport={{ once: false, amount: 0.1 }}
+        className="md:col-span-2 text-gray-500 p-2"
+      >
+        <motion.p variants={fadeIn("up", "tween", 0.1, 0.9)}>
+          At Avisa Swasthya, we are dedicated to transforming healthcare through
+          innovation and compassion. With our advanced diagnostic solutions, we
+          ensure accurate and accessible testing for all. From comprehensive
+          prenatal and postnatal care to health education and community
+          outreach, our mission is to empower individuals, families, and
+          communities with personalized healthcare solutions for a healthier
+          future.
+        </motion.p>
+      </motion.div>
 
-      <motion.span 
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      // viewport={{ once: false, amount: 0.1 }}
-      className="text-center md:text-3xl text-2xl  text-primary font-bold col-span-4 py-4">
+      <motion.span
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        // viewport={{ once: false, amount: 0.1 }}
+        className="text-center md:text-3xl text-2xl  text-primary font-bold col-span-4 py-4"
+      >
         <motion.p
-      //  variants={fadeIn('up', 'tween', 0.2, 1)}
-        >OUR SERVICES</motion.p>
-        
+        //  variants={fadeIn('up', 'tween', 0.2, 1)}
+        >
+          OUR SERVICES
+        </motion.p>
       </motion.span>
-      <motion.div 
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      // viewport={{ once: false, amount: 0.1 }}
-      className="grid md:grid-cols-4 grid-cols-1 col-span-3 text-sm font-semibold text-white">
-       <Link
-       href="/services/avisa_swasthya_hemoglobinopathy"
-       >
-        <div 
-        // variants={fadeIn('up', 'tween', 0.1, 0.9)}
-        className="bg-primary p-4 flex flex-col py-8  hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500">
-          <Image 
-          src="/1.png"
-          alt="Avisa Swasthya Hemoglobinopathy"
-          width="75"
-          height="75"
-          className="mx-auto my-4 "
-          />
-        Avisa Swasthya<br/> Hemoglobinopathy
-        </div>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        // viewport={{ once: false, amount: 0.1 }}
+        className="grid md:grid-cols-4 grid-cols-1 col-span-3 text-sm font-semibold text-white"
+      >
+        <Link href="/services/avisa_swasthya_hemoglobinopathy">
+          <div
+            // variants={fadeIn('up', 'tween', 0.1, 0.9)}
+            className="bg-primary p-4 flex flex-col py-8  hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500"
+          >
+            <Image
+              src="/1.png"
+              alt="Avisa Swasthya Hemoglobinopathy"
+              width="75"
+              height="75"
+              className="mx-auto my-4 "
+            />
+            Avisa Swasthya
+            <br /> Hemoglobinopathy
+          </div>
         </Link>
 
-        <Link
-        href="/services/avisa_swasthya_maternal_and_child_care"
-        >
-        <div 
-        // variants={fadeIn('up', 'tween', 0.1, 0.9)}
-        className="bg-[#C44D4F] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500">
-        <Image 
-          src="/2.png"
-          alt="Avisa Swasthya Hemoglobinopathy"
-          width="75"
-          height="75"
-          className="mx-auto my-4"
-          />
-        Avisa Swasthya Maternal<br/> and Child Care 
-        </div>
+        <Link href="/services/avisa_swasthya_maternal_and_child_care">
+          <div
+            // variants={fadeIn('up', 'tween', 0.1, 0.9)}
+            className="bg-[#C44D4F] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500"
+          >
+            <Image
+              src="/2.png"
+              alt="Avisa Swasthya Hemoglobinopathy"
+              width="75"
+              height="75"
+              className="mx-auto my-4"
+            />
+            Avisa Swasthya Maternal
+            <br /> and Child Care
+          </div>
         </Link>
 
-
-        <Link
-        href="/services/avisa_swasthya_health_and_care_camps"
-        >
-        <div 
-        // variants={fadeIn('up', 'tween', 0.1, 0.9)}
-        className="bg-[#CB6163] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500">
-        <Image 
-          src="/4.png"
-          alt="Avisa Swasthya Hemoglobinopathy"
-          width="75"
-          height="75"
-          className="mx-auto my-4"
-          />
-        Avisa Swasthya Health and Care Camps
-        </div>
+        <Link href="/services/avisa_swasthya_health_and_care_camps">
+          <div
+            // variants={fadeIn('up', 'tween', 0.1, 0.9)}
+            className="bg-[#CB6163] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500"
+          >
+            <Image
+              src="/4.png"
+              alt="Avisa Swasthya Hemoglobinopathy"
+              width="75"
+              height="75"
+              className="mx-auto my-4"
+            />
+            Avisa Swasthya Health and
+            <br /> Care Camps
+          </div>
         </Link>
-        
-        <Link
-        href="/services/avisa_swasthya_awareness"
-        >
-        <div 
-        // variants={fadeIn('up', 'tween', 0.1, 0.9)}
-        className="bg-[#D17576] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500">
-        <Image 
-          src="/3.png"
-          alt="Avisa Swasthya Hemoglobinopathy"
-          width="75"
-          height="75"
-          className="mx-auto my-4"
-          />
-        Avisa Swasthya <br/> Awareness
-        </div>
+
+        <Link href="/services/avisa_swasthya_awareness">
+          <div
+            // variants={fadeIn('up', 'tween', 0.1, 0.9)}
+            className="bg-[#D17576] p-4 flex flex-col py-8 hover:transform hover:scale-y-110 hover:scale-x-110 hover:transform-origin-bottom duration-500"
+          >
+            <Image
+              src="/3.png"
+              alt="Avisa Swasthya Hemoglobinopathy"
+              width="75"
+              height="75"
+              className="mx-auto my-4"
+            />
+            Avisa Swasthya <br /> Awareness
+          </div>
         </Link>
       </motion.div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
-"use client"
-import {useEffect, useState} from 'react'
-import Image from 'next/image'
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Footer from './components/Footer';
-import {HiOutlineArrowCircleLeft, HiOutlineArrowCircleRight} from 'react-icons/hi'
-import 'keen-slider/keen-slider.min.css'
-import { useKeenSlider } from 'keen-slider/react' 
-import useEmblaCarousel from 'embla-carousel-react'
-import Autoplay from 'embla-carousel-autoplay'
-import {motion} from 'framer-motion'
-import { navVariants, textVariant, textContainer, staggerContainer } from './utils/motion';
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import {
+  HiOutlineArrowCircleLeft,
+  HiOutlineArrowCircleRight,
+} from "react-icons/hi";
+import "keen-slider/keen-slider.min.css";
+import { useKeenSlider } from "keen-slider/react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { motion } from "framer-motion";
+import {
+  navVariants,
+  textVariant,
+  textContainer,
+  staggerContainer,
+} from "./utils/motion";
+import {AiOutlineInfoCircle} from 'react-icons/ai'
+import Link from 'next/link'
+
+
 
 export default function Home() {
-
   const CarouselData = [
     {
       id: 1,
       img: "bg-[url('./../../public/pioneer.jpg')]",
       text: "Thalassaemia Solutions: 'Pioneering Breakthroughs for Optimal Well-being'",
-    }, 
+    },
     {
       id: 2,
       img: "bg-[url('./../../public/testing.jpg')]",
@@ -29,47 +40,112 @@ export default function Home() {
       id: 3,
       img: "bg-[url('./../../public/ai_adv.jpeg')]",
       text: "The Future of Thalassaemia Diagnosis: Leveraging AI for precise results",
-    }
-  ]
+    },
+  ];
 
   const [img, setImg] = useState(CarouselData[0].img);
-  const [text, setText] = useState(CarouselData[0].text)
+  const [text, setText] = useState(CarouselData[0].text);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setImg(CarouselData[(CarouselData.findIndex(item => item.img === img) + 1) % CarouselData.length].img);
-      setText(CarouselData[(CarouselData.findIndex(item => item.text === text) + 1) % CarouselData.length].text);
+      setImg(
+        CarouselData[
+          (CarouselData.findIndex((item) => item.img === img) + 1) %
+            CarouselData.length
+        ].img
+      );
+      setText(
+        CarouselData[
+          (CarouselData.findIndex((item) => item.text === text) + 1) %
+            CarouselData.length
+        ].text
+      );
     }, 3000);
 
     return () => {
       clearInterval(interval);
     };
   }, [CarouselData, img, text]);
-  
-  return (
-    <main className="text-black bg-white relative flex min-h-screen md:h-[1800px] h-[2800px] flex-col items-center md:px-24 px-6">
-      <Navbar/>
 
-      <div className="bg-transparent md:h-[62px] ">
-      </div>
-      
-      <div className={`relative md:w-full w-screen bg-center bg-no-repeat bg-contain md:h-[500px] h-[500px] ${img} object-scale-down md:object-cover md:mb-4`}>
-      <motion.div 
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: false, amount: 0.25 }}
-      className="container rounded-lg absolute top-0 bottom-0 left-0 right-0 m-auto w-4/5 h-fit bg-white/50">
-      <motion.span
-      variants={textVariant(1.1)}
-      className="md:text-3xl text-xl font-bold text-center h-fit absolute top-0 bottom-0 left-0 right-0 m-auto"
+  return (
+    <main className="text-black bg-white relative flex min-h-screen md:h-[2550px] h-[3300px] flex-col items-center md:px-24 px-6">
+      <Navbar />
+
+      <div className="bg-transparent md:h-[62px] "></div>
+
+      <div
+        className={`relative md:w-full w-screen bg-center bg-no-repeat bg-contain md:bg-cover md:h-[500px] h-[500px] ${img} md:mb-4`}
       >
-        {text}
-      </motion.span>
-      </motion.div>
-    </div>
-      <About/>
-      <Footer/>
+        <motion.div
+          variants={staggerContainer}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.25 }}
+          className="container rounded-lg absolute top-0 bottom-0 left-0 right-0 m-auto w-4/5 h-fit bg-white/50"
+        >
+          <motion.span
+            variants={textVariant(1.1)}
+            className="md:text-3xl text-xl font-bold text-center h-fit absolute top-0 bottom-0 left-0 right-0 m-auto"
+          >
+            {text}
+          </motion.span>
+        </motion.div>
+      </div>
+      <About />
+
+      <div>
+        <h1
+         className="md:text-3xl text-center my-4 py-8 text-2xl font-bold h-fit my-auto text-primary"
+        >Information Center</h1>
+
+        <iframe
+          height="260"
+          className="md:w-1/2 w-full mx-auto"
+          src="https://www.youtube.com/embed/-8OJKZYGohY?si=gxgWRliT3lCEQELj"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+
+<Link
+        href="/information_center"
+        >
+        <button
+        className="my-8 flex border-[1px] font-semibold rounded-full bg-primary text-white p-4 justify-center w-fit mx-auto"
+        >
+          <AiOutlineInfoCircle
+          fontSize={25}
+          className="mx-2"
+          />
+          Information Center
+        </button>
+        </Link>
+
+        <div
+        className="grid md:grid-cols-3 grid-cols-1 md:grid-rows-2 grid-rows-1 p-8 gap-8"
+        >
+        <img 
+        src="/newspaper.png"
+        className="col-span-2 row-span-2 rounded-md shadow-lg mx-auto"
+        alt="Newspaper"
+        />
+
+        <img 
+        src="/Twitter_post.png"
+        className="rounded-md shadow-lg row-span-1 mx-auto"
+        alt="Twitter"
+        />
+        <img 
+        src="/newspaper-2.png"
+        className=" rounded-md shadow-lg row-span-1 mx-auto"
+        alt="Newspaper"
+        />
+        </div>
+
+        
+      </div>
+      <Footer />
     </main>
-  )
+  );
 }
